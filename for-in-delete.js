cc2996520baba46abbd314e327919ad87aff1885
Root <robhunn74@gmail.com>
2018-06-21 01:22:34 -0500

    done!!!!!!!!!!!!!1

--- for-in-delete.js
+++ for-in-delete.js
@@ -11,17 +11,17 @@
   In the example below, we are accessing the property values. Uncomment the code below, run it and look at what prints in the console.
 */
 
-// var values = {
-//   one: 'These',
-//   two: ' are',
-//   three: ' the',
-//   four: ' property',
-//   five: ' values.'
-// } 
+ var values = {
+   one: 'These',
+   two: ' are',
+   three: ' the',
+   four: ' property',
+   five: ' values.'
+ } 
 
-// for(var key in values) {
-//   console.log(values[key])
-// }
+ for(var key in values) {
+   console.log(values[key])
+ }
 
 /*
   In this next example, we are accessing the property names themselves. Uncomment the code below, run it and look at what prints in the console.
@@ -40,9 +40,12 @@
 */
 
 function showValues( obj ) {
-  //Code Here
+  var str = "";
+  for(var prop in obj){
+  str = str + obj[prop]
+} 
+return str
 }
-
 
 
 ////////// PROBLEM 2 //////////
@@ -54,7 +57,14 @@ function showValues( obj ) {
 */
 
 //Code Here
-
+function greaterThan10(OoOo){
+  for(var key in OoOo){
+    if (OoOo[key] > 10){
+          OoOo[key]= 0
+    }
+  }
+  return OoOo
+}
 
 
 ////////// PROBLEM 3 //////////
